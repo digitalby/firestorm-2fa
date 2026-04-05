6b98c36f016dcd8f60d7bed88a5fbc89e894694d
digitalby <yury@digitalby.me>
2026-04-06 01:46:20 +0200

    Fix dev run: inline web-ext CLI args, correct Thunderbird binary path, bump transitive deps

--- packages/firefox/web-ext-run-config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('web-ext').WebExtRunOptions} */
-export default {
-  sourceDir: '.',
-  artifactsDir: '../../artifacts/firefox',
-  firefox: '/Applications/Firefox.app/Contents/MacOS/firefox',
-  firefoxProfile: '../../profiles/firefox-dev',
-  keepProfileChanges: true,
-  startUrl: ['about:debugging#/runtime/this-firefox'],
-};
--- packages/thunderbird/web-ext-run-config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('web-ext').WebExtRunOptions} */
-export default {
-  sourceDir: '.',
-  artifactsDir: '../../artifacts/thunderbird',
-  target: ['thunderbird-desktop'],
-  firefox: '/Applications/Thunderbird.app/Contents/MacOS/thunderbird-bin',
-  firefoxProfile: '../../profiles/thunderbird-dev',
-  keepProfileChanges: true,
-};
